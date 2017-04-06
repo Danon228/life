a7296e711dcef7f41bf230390801a4594a5bb54f
Danon228 <DanilR200151@gmail.com>
2017-04-06 21:53:14 +0300

    Программа работает остался интерфейс и все

--- game_of_life.py
+++ game_of_life.py
@@ -49,10 +49,11 @@ class Matrix:
             y_obj = y1
             return x_obj, y_obj
 
-    def refresh(self, list_of_obj):
-        self.matrix = [[None]*self.x for x in range(self.y)]
-        for obj in [list_of_obj]:
-            self.put_elements(obj, obj.cor_x, obj.cor_y)
+    def refresh(self, list):
+        self.list_of_objects = list
+        self.matrix = self.matrix = [[None]*self.y for x in range(self.x)]
+        for obj in (self.list_of_objects):
+            self.matrix[obj.cor_x][obj.cor_y] = obj
 
     def get_list_of_objects(self):
         self.list_of_objects = []
@@ -60,6 +61,7 @@ class Matrix:
             for y in range(HEIGHT):
                 if self.get_element(x, y):
                     self.list_of_objects.append(self.get_element(x, y))
+        return self.list_of_objects
 
 
 class Agent:
@@ -73,7 +75,7 @@ class Agent:
     def get_name(self):
         return self.name
 
-    def belong_class(self, obj, cls):
+    def belong_class( self, obj, cls):
         return isinstance(obj, cls)
 
     def give_life(self,count=10):
@@ -95,6 +97,7 @@ class Agent:
 
 
 class Controller:
+
     def delete_element(self, obj):
         self.list_of_objects.remove(obj)
 
@@ -115,38 +118,34 @@ class Controller:
             obj_1.take_energy(obj_1.energy)
             obj_2.take_energy(obj_1.energy)
 
-    def find_place(self, obj_1, obj_2=None):
-        x1, y1 = obj_1.cor_x, obj_1.cor_y
+    def find_place(self, obj_1, obj_2 = None):
         if obj_2:
+            x1, y1 = obj_1.cor_x, obj_1.cor_y
             x2, y2 = obj_2.cor_x, obj_2.cor_y
             x_free = 2 * x1 - x2
             y_free = 2 * y1 - y2
-            t = self.matrix.get_element(x_free, y_free)
+            t = False
         else:
-            t = True
             x_free = 0
             y_free = 0
-        free_places = [[0] * 2] * 8
-        if self.matrix.get_element(x_free, y_free) == 'Empty' or t:
-            free_places[0][0], free_places[0][1] = x1, y1 + 1
-            free_places[1][0], free_places[1][1] = x1 + 1, y1 + 1
-            free_places[2][0], free_places[2][1] = x1 + 1, y1
-            free_places[3][0], free_places[3][1] = x1 + 1, y1 - 1
-            free_places[4][0], free_places[4][1] = x1, y1 - 1
-            free_places[5][0], free_places[5][1] = x1 - 1, y1 - 1
-            free_places[6][0], free_places[6][1] = x1 - 1, y1
-            free_places[7][0], free_places[7][1] = x1 - 1, y1 + 1
-            for k in range(7):
-                x = free_places[k][0]
-                y = free_places[k][1]
-                if self.matrix.get_element(x, y) == 'Empty' or self.matrix.get_element(x, y) >= 0:
-                    free_places.remove([x, y])
-                if len(free_places) != 0:
-                    line = choice(free_places)
-                    x_free = line[0]
-                    y_free = line[1]
-                else:
-                    x_free, y_free = None, None
+            t = True
+        if self.matrix.get_element(x_free, y_free) == 'Empty' or t or self.matrix.get_element(x_free, y_free):
+            free_places = []
+            x = obj_1.cor_x
+            y = obj_1.cor_y
+            for x1 in [-1, 0, 1]:
+                for y1 in [-1, 0, 1]:
+                    if x1 != 0 and y1 != 0:
+                        if not self.matrix.get_element(x + x1, y + y1) != 'Empty' and \
+                                not self.matrix.get_element(x + x1, y + y1):
+                            free_places.append([x + x1, y + y1])
+            if len(free_places):
+                line = choice(free_places)
+                x_free = line[0]
+                y_free = line[1]
+            else:
+                x_free = None
+                y_free = None
         return x_free, y_free
 
     def get_list_of_neighbour(self, obj):
@@ -156,132 +155,135 @@ class Controller:
         for x1 in [-1, 0, 1]:
             for y1 in [-1, 0, 1]:
                 if x1 != 0 and y1 != 0:
-                    if self.matrix.get_element(x + x1, y + y1) != 'Empty' and \
-                            self.matrix.get_element(x + x1, y + y1):
-                        list_of_neighbour.append(self.matrix.get_element(x + x1, y + y1))
+                    if self.matrix.get_element(x+x1, y+y1) and self.matrix.get_element(x+x1, y+y1) != 'Empty':
+                        list_of_neighbour.append(self.matrix.get_element(x+x1, y+y1))
         if len(list_of_neighbour):
-            return None
-        else:
             return choice(list_of_neighbour)
+        else:
+            return None
 
-    def create_obj(self, cls, count):
+    def create_objects(self, count, cls):
         i = 0
         while i != count:
             obj = cls()
             x_cor, y_cor = self.matrix.put_elements(obj)
-            if x_cor and y_cor:
+            if x_cor or y_cor:
                 obj.cor_x = x_cor
                 obj.cor_y = y_cor
                 i += 1
 
     def start_game(self, matrix):
         self.matrix = matrix
-        self.create_obj(Animal, COUNT_A)
-        self.create_obj(Predator, COUNT_P)
-        self.create_obj(Food, COUNT_F)
-        self.list_of_objects = self.matrix.list_of_objects()
-        for obj in [self.list_of_objects]:
-            neighbour = self.get_list_of_neighbour(obj)
-            if not obj.is_worked:
-                if not neighbour:
-                    if obj.belong_class(obj, Animal):
+        self.create_objects(COUNT_A, Animal)
+        self.create_objects(COUNT_P, Predator)
+        self.create_objects(COUNT_F, Food)
+        i = 0
+        while i != 100:
+            self.list_of_objects = self.matrix.get_list_of_objects()
+            for obj in (self.list_of_objects):
+                neighbour = self.get_list_of_neighbour(obj)
+                if not obj.is_worked:
+                    if not neighbour:
                         x_free, y_free = self.find_place(obj)
                         if x_free and y_free:
-                            child = Animal()
-                            child.cor_x, child.cor_y = self.matrix.put_elements(child, x_free, y_free)
-                            obj.take_energy(obj.energy)
-                            child.life = obj.life/2
-                            obj.take_life(obj.life/2)
+                            if obj.belong_class(obj, Animal):
+                                child = Animal()
+                                child.cor_x, child.cor_y = self.matrix.put_elements(child, x_free, y_free)
+                                self.list_of_objects.append(child)
+                                child.energy = obj.energy/2
+                                child.life = obj.life/2
+                                obj.take_life(obj.life/2)
+                                obj.take.energy(obj.energy)
+                            if obj.belong_class(obj, Predator):
+                                child = Predator()
+                                child.cor_x, child.cor_y = self.matrix.put_elements(child, x_free, y_free)
+                                self.list_of_objects.append(child)
+                                child.energy = obj.energy
+                                child.life = obj.life/2
+                                obj.take_life(obj.life/2)
+                                obj.take.energy(obj.energy)
+                            if obj.belong_class(obj, Food):
+                                child = Food()
+                                child.cor_x, child.cor_y = self.matrix.put_elements(child, x_free, y_free)
+                                self.list_of_objects.append(child)
+                                child.life = obj.life / 2
+                                obj.take_life(obj.life / 2)
                         else:
-                            obj.give_energy(20)
-                            obj.take_life()
-                    if obj.belong_class(obj, Predator):
-                        x_free, y_free = self.find_place(obj)
-                        if x_free and y_free:
-                            child = Predator()
-                            child.cor_x, child.cor_y = self.matrix.put_elements(child, x_free, y_free)
-                            obj.take_energy(obj.energy)
-                            child.life = obj.life/2
-                            obj.take_life(obj.life/2)
-                        else:
-                            obj.give_energy(20)
-                            obj.take_life()
-                    if obj.belong_class(obj, Food):
-                        x_free, y_free = self.find_place(obj)
-                        if x_free and y_free:
-                            child = Food()
-                            child.cor_x, child.cor_y = self.matrix.put_elements(child, x_free, y_free)
-                            child.life = obj.life / 2
-                            obj.take_life(obj.life / 2)
-                        else:
-                            obj.take_life(50)
-                else:
-                    if obj.belong_class(obj, Animal):
-                        if neighbour.belong_class(neighbour, Animal):
-                            if obj.energy > neighbour.energy:
-                                self.attack_1(obj, neighbour)
-                                obj.is_worked = True
+                            if obj.belong_class(obj, Food):
+                                obj.give_life(50)
                             else:
-                                x_end, y_end = self.find_place(obj, neighbour)
-                                if x_end and y_end:
-                                    obj.travel(x_end, y_end)
-                                    obj.is_worked = True
-                                else:
-                                    obj.give_energy(20)
-                                    obj.take_life(10)
-                                    if obj.life <= 0:
-                                        self.delete_element(obj)
-                        if neighbour.belong_class(neighbour, Food):
-                            if obj.energy > 0:
-                                if neighbour.life <= obj.energy:
-                                    self.delete_element(neighbour)
-                                    obj.take_energy(neighbour.life)
-                                    obj.give_life(neighbour.life)
-                                    obj.is_worked = True
-                                else:
-                                    neighbour.take_life(obj.energy)
-                                    obj.give_life(obj.energy)
-                                    obj.take_energy(obj.energy)
-                                    obj.is_worked = True
-                            else:
-                                obj.give_energy(20)
-                                obj.take_life()
-                        if neighbour.belong_class(neighbour, Predator):
-                            if obj.energy >= 10:
-                                x_end, y_end = self.find_place(obj, neighbour)
-                                if x_end and y_end:
-                                    obj.travel(x_end, y_end)
-                            else:
-                                obj.give_energy(20)
-                                obj.take_life(10)
-                    if obj.belong_class(obj, Predator):
-                        if neighbour.belong_class(neighbour, Animal):
-                            if obj.energy > 0:
-                                self.attack_2(obj, neighbour)
-                                obj.is_worked = True
-                            else:
-                                obj.give_energy(20)
-                                obj.take_life(10)
+                                obj.give_energy(30)
+                                obj.take_life(5)
                                 if obj.life <= 0:
                                     self.delete_element(obj)
-                        if neighbour.belong_class(neighbour, Predator):
-                            if obj.energy > neighbour.energy:
-                                self.attack_1(obj, neighbour)
-                                obj.is_worked = True
-                            else:
-                                x_end, y_end = self.find_place(obj, neighbour)
-                                if x_end and y_end:
-                                    obj.travel(x_end, y_end)
+                    else:
+                        if obj.belong_class(obj, Animal):
+                            if neighbour.belong_class(neighbour, Animal):
+                                if obj.energy > neighbour.energy:
+                                    self.attack_1(obj, neighbour)
+                                    obj.is_worked = True
+                                else:
+                                    x_end, y_end = self.find_place(obj, neighbour)
+                                    if x_end and y_end:
+                                        obj.travel(x_end, y_end)
+                                        obj.is_worked = True
+                                    else:
+                                        obj.give_energy(20)
+                                        obj.take_life(10)
+                                        if obj.life <= 0:
+                                            self.delete_element(obj)
+                            if neighbour.belong_class(neighbour, Food):
+                                if obj.energy > 0:
+                                    if neighbour.life <= obj.energy:
+                                        self.delete_element(neighbour)
+                                        obj.take_energy(neighbour.life)
+                                        obj.give_life(neighbour.life)
+                                        obj.is_worked = True
+                                    else:
+                                        neighbour.take_life(obj.energy)
+                                        obj.give_life(obj.energy)
+                                        obj.take_energy(obj.energy)
+                                        obj.is_worked = True
+                                else:
+                                    obj.give_energy(20)
+                                    obj.take_life()
+                            if neighbour.belong_class(neighbour, Predator):
+                                if obj.energy >= 10:
+                                    x_end, y_end = self.find_place(obj, neighbour)
+                                    if x_end and y_end:
+                                        obj.travel(x_end, y_end)
+                                else:
+                                    obj.give_energy(20)
+                                    obj.take_life(10)
+                        if obj.belong_class(obj, Predator):
+                            if neighbour.belong_class(neighbour, Animal):
+                                if obj.energy > 0:
+                                    self.attack_2(obj, neighbour)
                                     obj.is_worked = True
                                 else:
                                     obj.give_energy(20)
                                     obj.take_life(10)
                                     if obj.life <= 0:
                                         self.delete_element(obj)
-                    if obj.belong_class(obj, Food):
-                        obj.give_life(30)
-        self.matrix.refresh(self.list_of_objects)
-        print(self.matrix)
+                            if neighbour.belong_class(neighbour, Predator):
+                                if obj.energy > neighbour.energy:
+                                    self.attack_1(obj, neighbour)
+                                    obj.is_worked = True
+                                else:
+                                    x_end, y_end = self.find_place(obj, neighbour)
+                                    if x_end and y_end:
+                                        obj.travel(x_end, y_end)
+                                        obj.is_worked = True
+                                    else:
+                                        obj.give_energy(20)
+                                        obj.take_life(10)
+                                        if obj.life <= 0:
+                                            self.delete_element(obj)
+                        if obj.belong_class(obj, Food):
+                            obj.give_life(30)
+            self.matrix.refresh(self.list_of_objects)
+            print(self.matrix)
+            i += 1
 
 
 class Animal(Agent):               
